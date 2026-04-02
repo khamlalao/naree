be9753415eddf59b1f990d3bad019fcd1ba7ed44
khamlalao <oppo93897429@gmail.com>
2026-04-02 15:06:43 +0700

    update

--- new-en/inc_search_product.php
+++ new-en/inc_search_product.php
@@ -2,7 +2,7 @@
       <div class="product_search post">
       <form action="search.php" method="get" enctype="multipart/form-data">
       <input type="text" placeholder="Key of Product Code, Product Name..."  class="txt-box" name="keyword" id="keyword" >
-        <label type="image" src="images/icon/i_search.png" width="40" height="40" class="txt-btn ">
+        <input type="image" src="images/icon/i_search.png" width="40" height="40" class="txt-btn ">
 </form>
       </div>
       <!--// product search--> 
